95f2ab157cbc6cca6d068a18e1bd1d0322106241
abhinavpulluri <abhinavpulluri20@gmail.com>
2025-06-05 19:08:54 +0530

    added p2

---
+++ p2/script.js
@@ -0,0 +1,24 @@
+const showLogin = () => {
+    let str= `
+    <h1>Login form</h1>
+    <p><input type="text" id="txtEmail" placeholder="Email"></p>
+    <p><input type="password" id="txtPassword" placeholder="Password"></p>
+    <p><button>Login</button></p>
+    <p><button onclick="showRegister()">Create account</button></p>
+    `
+
+    root.innerHTML = str;
+}
+
+const showRegister = () => {
+    let str= `
+    <h1>Registration form</h1>
+    <p><input type="text" id="txtName" placeholder="Name"></p>
+    <p><input type="text" id="txtEmail" placeholder="Email"></p>
+    <p><input type="password" id="txtPassword" placeholder="Password"></p>
+    <p><button>Register</button></p>
+    <p><button onclick="showLogin()">Already have an account?</button></p>
+    `
+
+    root.innerHTML = str;
+}
